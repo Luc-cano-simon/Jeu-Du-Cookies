7054b37f8515b4ec2dc984fd85381e94112da65b
luc <luc.cano@hotmail.fr>
2018-06-19 14:27:57 +0200

    Exo 7

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ var multi = 1;
 
 $("#button").click(function()
 {
+
 	i = i+1*multi; 
 	$("#Score").html(i); 
 });
 
 $("#multiply").click(function()
 {
-	multi = multi+1;
+	
+	if(i >= 50){
+		multi = multi + 1;
+		i = i - 50;
+
+		
+
+	}else{
+		alert("point insuffisant")
+	}
+	
+	$("#Score").html(i); 
 
 }
 
